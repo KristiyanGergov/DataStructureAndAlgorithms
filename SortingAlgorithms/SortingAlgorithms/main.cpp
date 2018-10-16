4ede5994c3bda6d96ee5f7fe83f3b5243769e627
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-10-16 10:37:02 +0300

    Finished merge sort

--- SortingAlgorithms/SortingAlgorithms/main.cpp
+++ SortingAlgorithms/SortingAlgorithms/main.cpp
@@ -89,16 +89,49 @@ void merge(int* arr, int l, int m, int r) {
 
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
-	for (j = 0; i < n2; i++)
-		R[i] = arr[m + 1 + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
 
 	i = 0;
 	j = 0;
 	k = l;
 
+
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+	delete[] L;
+	delete[] R;
 }
 
-void mergeSort(int* arr, int l, int r) {
+void mergeSort(int arr[], int l, int r) {
+
 	if (l < r)
 	{
 		int m = l + (r - l) / 2;
@@ -112,8 +145,11 @@ void mergeSort(int* arr, int l, int r) {
 //Merge
 
 int main() {
-	int arr[] = { 9, 19 , 2, 15, 23, 5 };
-	insertionSort(arr, 6);
+
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	mergeSort(arr, 0, 5);
 
 	print(arr, 6);
 
