795e1eeffb6d7e197d0bdb046537055b73cb9bfd
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-10-16 08:49:35 +0300

    Finished Hw2

--- Hw2/Task4/main.cpp
+++ Hw2/Task4/main.cpp
@@ -1,38 +1,81 @@
 #include <iostream>
+
 using namespace std;
 
+long _mergeSort(long arr[], long temp[], long left, long right);
+long merge(long arr[], long temp[], long left, long mid, long right);
+
+long mergeSort(long arr[], long array_size)
+{
+	long* temp = (long*)malloc(sizeof(long) * array_size);
+	return _mergeSort(arr, temp, 0, array_size - 1);
+}
+
+long _mergeSort(long arr[], long temp[], long left, long right)
+{
+	long mid, inv_count = 0;
+	if (right > left) {
+		mid = (right + left) / 2;
+
+		inv_count = _mergeSort(arr, temp, left, mid);
+		inv_count += _mergeSort(arr, temp, mid + 1, right);
+
+
+		inv_count += merge(arr, temp, left, mid + 1, right);
+	}
+	return inv_count;
+}
+
+long merge(long arr[], long temp[], long left, long mid, long right)
+{
+	long i, j, k;
+	long inv_count = 0;
+
+	i = left; 
+	j = mid; 
+	k = left; 
+	while ((i <= mid - 1) && (j <= right)) {
+		if (arr[i] <= arr[j]) {
+			temp[k++] = arr[i++];
+		}
+		else {
+			temp[k++] = arr[j++];
+
+			inv_count = inv_count + (mid - i);
+		}
+	}
+
+	while (i <= mid - 1)
+		temp[k++] = arr[i++];
+
+	while (j <= right)
+		temp[k++] = arr[j++];
+
+	for (i = left; i <= right; i++)
+		arr[i] = temp[i];
+
+	return inv_count;
+}
+
+
 int main() {
-	int q, n;
+	long q, n;
 
 	cin >> q;
 
-	for (int i = 0; i < q; i++)
+	for (long i = 0; i < q; i++)
 	{
 		cin >> n;
-		int* arr = new int[n];
-		int count = 0;
-		int maxElement = 0;
+		long* arr = new long[n];
 
-		for (int j = 0; j < n; j++)
+		for (long j = 0; j < n; j++)
 		{
-			int num;
+			long num;
 			cin >> num;
 			arr[j] = num;
-
-			if (num >= maxElement)
-			{
-				maxElement = num;
-				continue;
-			}
-
-			for (int k = 0; k < j; k++)
-			{
-				if (arr[k] > arr[j])
-				{
-					count++;
-				}
-			}
 		}
+
+		long count = mergeSort(arr, n);
 
 		cout << count << endl;
 		delete[] arr;
