825d4237bc015bbe5c16fa23b21c5edeed110364
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-11-07 21:52:05 +0200

    Finished task hw03-task03

--- Hw4/Task03/main.cpp
+++ Hw4/Task03/main.cpp
@@ -1,121 +1,27 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-template <typename T>
 struct Node {
-	T data;
-	Node<T>* next;
-	bool initialized;
+	int data;
+	Node* next;
 };
-
-template <typename T>
-class LinkedList {
-private:
-	Node<T> head;
-	Node<T> tail;
-	int size;
-public:
-	void add(T data);
-	int getAt(int index);
-	int getSize();
-	int removeLast();
-};
-
-template <typename T>
-void LinkedList<T>::add(T data)
-{
-	Node<T>* temp = new Node<T>();
-	temp->data = data;
-	temp->next = nullptr;
-	temp->initialized = true;
-	this->size++;
-	if (head.initialized)
-	{
-		Node<T>* curr = &head;
-		while (curr->next)
-			curr = curr->next;
-
-		curr->next = temp;
-		tail = *temp;
-	}
-	else
-	{
-		head = *temp;
-		tail = *temp;
-	}
-
-}
-
-template <typename T>
-int LinkedList<T>::getAt(int index)
-{
-	Node<T> curr = head;
-	for (int i = 0; i < index; i++)
-	{
-		if (curr.next)
-		{
-			curr = *curr.next;
-		}
-	}
-	return curr.data;
-}
-
-template <typename T>
-int LinkedList<T>::getSize()
-{
-	return this->size;
-}
-
-
-class SnowMan {
-private:
-	LinkedList<int> balls;
-public:
-	LinkedList<int> getBalls();
-};
-
-LinkedList<int> SnowMan::getBalls()
-{
-	return this->balls;
-}
-
-template <typename T>
-int LinkedList<T>::removeLast()
-{
-	Node<T>* current;
-	Node<T>* previous = nullptr;
-	int res = tail.data;
-	current = &head;
-
-	while (current->next)
-	{
-		previous = current;
-		current = current->next;
-	}
-
-	tail = *previous;
-	previous->next->initialized = false;
-	tail.next = nullptr;
-	delete current;
-
-	return res;
-}
 
 int main() {
 	int n;
 	cin >> n;
 
-	LinkedList<LinkedList<int>> results = LinkedList<LinkedList<int>>();
+	Node* balls = new Node[n + 1]();
+	Node node;
+	node.data = 0;
+	node.next = nullptr;
+	balls[0] = node;
 
-	LinkedList<int> list = LinkedList<int>();
-	list.add(0);
-	int result = 0;
+	long sum = 0;
+	int* arr = new int[n + 1];
+	arr[0] = 0;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		int p, m;
 		cin >> p;
@@ -123,16 +29,20 @@ int main() {
 
 		if (m != 0)
 		{
-			int test = list.getAt(p) + m;
-			list.add(test);
+			node.data = m;
+			node.next = &balls[p];
+			balls[i] = node;
+			arr[i] = arr[p] + m;
+			sum += arr[i];
 		}
 		else
 		{
-			int test = list.getAt(list.getSize()) - (list.getAt(list.getSize()) - list.getAt(list.getSize() - 2));
-			list.add(test);
+			balls[i] = *balls[p].next;
+			arr[i] = arr[p] - balls[p].data;
+			sum += arr[i];
 		}
 	}
+	cout << sum;
 
 	return 0;
 }
-
