5a9fb48ee9fb21490dd4f0f71445df03df09c39b
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-11-15 07:51:08 +0200

    Changed task1 to work with stack

--- Hw5 - Stack & Queue/Task1/main.cpp
+++ Hw5 - Stack & Queue/Task1/main.cpp
@@ -1,20 +1,101 @@
 #include <iostream>
+
 using namespace std;
+
+template <typename T>
+struct Node {
+	T data;
+	Node* next;
+};
+
+template <typename T>
+class stack {
+private:
+	int size;
+	Node<T>* head;
+public:
+	stack();
+	void push(T data);
+	bool pop();
+	T top();
+	bool empty();
+};
+
+template<typename T>
+stack<T>::stack()
+{
+	this->size = 0;
+}
+
+template<typename T>
+void stack<T>::push(T data)
+{
+	Node<T>* curr = new Node<T>();
+	curr->data = data;
+	curr->next = head;
+	head = curr;
+	size++;
+}
+
+template<typename T>
+bool stack<T>::pop()
+{
+	if (size == 0)
+		return false;
+
+	Node<T>* curr = head;
+	head = head->next;
+	delete curr;
+	size--;
+	return true;
+}
+
+template<typename T>
+T stack<T>::top()
+{
+	return head->data;
+}
+
+template<typename T>
+bool stack<T>::empty()
+{
+	return this->size == 0;
+}
+
 int main() {
 	int n;
+
 	cin >> n;
+
 	int addend = 2;
 	int size = n;
-	int top = n;
-	int bot = 1;
+
+	stack<int> res;
+
+	res.push(1);
+	res.push(n);
+
 	while (size > 1)
 	{
-		if (size % 2 == 0) top -= addend / 2;
-		else bot = bot += addend;
+		int first = res.top();
+		res.pop();
+		int second = res.top();
+		res.pop();
 
-		addend *= 2;
+		if (size % 2 == 0)
+			first -= addend / 2;
+		else
+			second += addend;
+
+
+		res.push(second);
+		res.push(first);
+
 		size /= 2;
+		addend *= 2;
 	}
-	cout << top;
+
+	cout << res.top();
+
 	return 0;
 }
