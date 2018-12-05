e5a6bbc9732233bc23d7b280176de53a55f3d895
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-12-05 11:40:30 +0200

    Started hw08

--- Week 07 - Tree Algorithms/Homework/Task3/main.cpp
+++ Week 07 - Tree Algorithms/Homework/Task3/main.cpp
@@ -1,20 +1,61 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include <queue>
+
 using namespace std;
 
+// A utility function to add an edge in an 
+// undirected graph. 
+void addEdge(vector<int> adj[], int u, int v)
+{
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
 
-int main() {
+// A utility function to print the adjacency list 
+// representation of graph 
+void printGraph(vector<int> adj[], int V)
+{
+	for (int v = 0; v < V; ++v)
+	{
+		cout << "\n Adjacency list of vertex "
+			<< v << "\n head ";
+		for (auto x : adj[v])
+			cout << "-> " << x;
+		printf("\n");
+	}
+}
 
+// Driver code 
+int main()
+{
 	int n, m;
 
-	for (int i = 0; i < length; i++)
+	cin >> n;
+	cin >> m;
+
+	vector<int> adj[n];
+
+	queue<bool> coffees;
+
+	for (int i = 0; i < n; i++)
 	{
+		bool hasCoffee;
+		cin >> hasCoffee;
+
+		coffees.push(hasCoffee);
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		int vertice, edge;
+
+		cin >> vertice;
+		cin >> edge;
+		addEdge(adj, vertice, edge);
 
 	}
 
-
+	printGraph(adj, n - 1);
 	return 0;
 }
